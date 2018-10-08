403da0ff6ac57d7ae47303c8ffec3cc95b195e69
Sam Xu <547778494@qq.com>
2018-10-08 14:18:23 +0800

    Update IDB-api.js

--- IDB-api.js
+++ IDB-api.js
@@ -8,34 +8,19 @@
  */
 
 /* eslint-disable */
-const defOption = {
-    dbName: 'dms_mobile_mart',
-    version: 1,
-    errHandler: function (err) {
-        console.log('打开数据库失败', err)
-    },
-    upgradeHandler: function (e) {
-        console.log('数据库升级成功', e)
-        let db = e.target.result
-        if (!db.objectStoreNames.contains(this.storeName)) {
-            let objectStore = db.createObjectStore(this.storeName, {
-                keyPath: 'id',
-                autoIncrement: true
-            })
-            objectStore.createIndex('usertokenIndex', 'usertoken', { unique: true })
-            objectStore.createIndex('nonstandardIndex', 'nonstandard', { unique: false })
-            objectStore.createIndex('partsIndex', 'parts', { unique: false })
-        }
-    }
+const defOptions = {
+    dbName: '',
+    version: 1
 }
 
-const IDB = (options) => {
-    this.dbName = options.dbName || defOption.dbName
-    this.version = options.version || defOption.version
-    this.storeName = options.storeName || defOption.storeName
-    this.errHandler = options.errHandler || defOption.errHandler
-    this.upgradeHandler = options.upgradeHandler || defOption.upgradeHandler
+const IDB = function (options) {
+    this.dbName = options.dbName || defOptions.dbName
+    this.version = options.version || defOptions.version
+    this.storeList = options.storeList || []
     this.dbInstance = {}
+    this.setStoreName = (name) => {
+        this.storeName = name
+    }
 }
 
 IDB.prototype = {
@@ -43,72 +28,80 @@ IDB.prototype = {
      * 连接数据库
      * @returns {Function} cb 回调
      */
-    open: (cb) => {
+    open: function (cb) {
         let request = window.indexedDB.open(this.dbName, this.version)
-        request.onerror = this.errHandler
-        request.onupgradeneeded = this.upgradeHandler.bind(this)
+        request.onerror = (err) => {
+            console.log('打开数据库失败', err)
+        }
+        request.onupgradeneeded = (e) => {
+            console.log('数据库升级成功', e)
+            let db = e.target.result
+            if (this.storeList && this.storeList.length > 0) {
+                this.storeList.forEach((item) => {
+                    if (!db.objectStoreNames.contains(item)) {
+                        let objectStore = db.createObjectStore(item, {
+                            keyPath: 'id',
+                            autoIncrement: true
+                        })
+                        objectStore.createIndex('usertokenIndex', 'usertoken', { unique: true })
+                    }
+                })
+            }
+        }
         request.onsuccess = (e) => {
-            // console.log('打开数据库成功', e)
+            console.log('打开数据库成功', e)
             this.dbInstance = request.result
             if (cb && typeof cb === 'function') cb()
         }
     },
     // 获取表对象
-    getObjectStore: (mode) => {
+    getObjectStore: function (mode) {
         mode = mode || 'readonly'
         let txn = this.dbInstance.transaction([this.storeName], mode)
         let store = txn.objectStore(this.storeName)
         return store
     },
     // 增、改
-    save: (data, cb) => {
-        IDB.prototype.open(function () {
-            let store, request, mode
-            mode = 'readwrite'
-            store = IDB.prototype.getObjectStore(mode)
-            request = data.id ? store.put(data) : store.add(data)
-            if (cb && typeof cb === 'function') {
-                request.onsuccess = cb
-                request.onerror = cb
-            }
-        })
+    save: function (data, cb) {
+        let store, request, mode
+        mode = 'readwrite'
+        store = this.getObjectStore(mode)
+        request = data.id ? store.put(data) : store.add(data)
+        if (cb && typeof cb === 'function') {
+            request.onsuccess = cb
+            request.onerror = cb
+        }
     },
     // 查表
-    get: (method, cb) => {
-        IDB.prototype.open(function () {
-            let request, _method
-            let store = IDB.prototype.getObjectStore()
-            if (method.id) {
-                _method = parseInt(method.id)
-                request = store.get(_method)
-            } else {
-                Object.keys(method).forEach((item) => {
-                    _method = method[item]
-                    request = store.index(item).get(_method)
-                })
-            }
-            request.onsuccess = function (e) {
-                if (cb && typeof cb === 'function') cb(e.target.result)
-            }
-        })
+    get: function (method, cb) {
+        let request, _method
+        let store = this.getObjectStore()
+        if (method.id) {
+            _method = parseInt(method.id)
+            request = store.get(_method)
+        } else {
+            Object.keys(method).forEach((item) => {
+                _method = method[item]
+                request = store.index(item).get(_method)
+            })
+        }
+        request.onsuccess = function (e) {
+            if (cb && typeof cb === 'function') cb(e.target.result)
+        }
     },
     // 删
-    delete: (id, cb) => {
-        IDB.prototype.open(function () {
-            let mode = 'readwrite'
-            let store = IDB.prototype.getObjectStore(mode)
-            let request = store.delete(id)
-            if (cb && typeof cb === 'function') request.onsuccess = cb
-        })
+    delete: function (id, cb) {
+        let mode = 'readwrite'
+        let store = this.getObjectStore(mode)
+        let request = store.delete(id)
+        if (cb && typeof cb === 'function') request.onsuccess = cb
     },
     // 清空表
-    clear: (cb) => {
-        IDB.prototype.open(function () {
-            let mode = 'readwrite'
-            let store = IDB.prototype.getObjectStore(mode)
-            let request = store.clear()
-            if (cb && typeof cb === 'function') request.onsuccess = cb
-        })
+    clear: function (cb) {
+        let mode = 'readwrite'
+        let store = this.getObjectStore(mode)
+        let request = store.clear()
+        if (cb && typeof cb === 'function') request.onsuccess = cb
     }
 }
 
